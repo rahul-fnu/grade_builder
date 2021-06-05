60611a34bc79742675b5a9d561bef65121d5a452
k19025136 <k19025136@kcl.ac.uk>
2021-06-05 21:23:30 +0300

    Add subjects api

---
+++ pages/api/subjects/index.js
@@ -0,0 +1,29 @@
+import dbConnect from '../../../utils/dbConnect';
+import Subject from '../../../models/Subject';
+
+dbConnect();
+
+export default async (req, res) => {
+    const { method } = req;
+    switch (method) {
+        case 'GET':
+            try {
+                const subjects = await Subject.find({})
+                res.status(200).json({success: true, data: subjects})
+            } catch (error) {
+                res.status(400).json({success: false});
+            }
+            break;
+        case 'POST':
+            try {
+                const subject = await Subject.create(req.body);
+                res.status(200).json({success: true, data: subject});
+            } catch (error) {
+                res.status(400).json({success: false});
+            }
+            break;
+        default:
+            res.status(400).json({success: false});
+            break;
+    }
+}
