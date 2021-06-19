6b237abbe0066bbf10d12213f40a61535aefa714
rahul-fnu <rahul-fnu@users.noreply.github.com>
2021-06-19 12:32:07 -0400

    Latex parser chnages made

--- react_components/question_components/prompt_card.js
+++ react_components/question_components/prompt_card.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import styles from '../../styles/Question.module.css';
 import Subpart from './subpart_card';
-import {latexParser} from '../../node_modules/latex-parser';
+require("//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css")
+var Latex = require('react-latex');
 export default class Prompt extends Component {
     render() {
-        const la = latexParser.parse(this.props.p.prompt).value[0].text
-        console.log(la)
         return (
             <div className = {styles.container}>
                 <div className={styles.prompt_card}>
-                    <p>{`Part ${this.props.p.part}: ${la}`}</p>
+                    <p>{`Part ${this.props.p.part}`}</p>
+                    <Latex>{this.props.p.prompt}</Latex>
                     <p>{`Points: ${this.props.p.marks}`}</p>
                     {this.props.p.text && <textarea></textarea>}
                     {(this.props.p.subparts.length) > 0 && 
