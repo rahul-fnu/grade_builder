fb5d60b02aac419ca6d5411f645742a57cac7192
rahul-fnu <rahul-fnu@users.noreply.github.com>
2021-07-18 11:19:13 -0400

    State fixed

--- react_components/question_components/question.js
+++ react_components/question_components/question.js
@@ -6,47 +6,21 @@ import Popup from '../../node_modules/reactjs-popup'
 import '../../node_modules/reactjs-popup/dist/index.css'
 // import { useSelector, useDispatch } from 'react-redux';
 // import { saveInput } from '../../store/user_input/action';
-import cookie from 'cookie'
-
-var ans = [];
-// const globalState = useSelector((st))
-
-
 export default class QuestionC extends Component {
-    // static async getInitialProps(ctx) {
-    //     const rest = await fetch(`http://${ctx.req.headers.host}/question/${ctx.query.question_id}`)
-
-    //     const initAns = cookies(rest).answers;
-    //     console.log(initAns)
-    //     return {
-    //       answers: initAns || {}
-    //     }
-    // }
-    
     constructor(props) {
         super(props);
-        console.log(document.cookie)
+        // console.log(document.cookie)
         this.state = {
             prompts: [],
-            answers: {},
-            perop: document.cookie
-            // answers: JSON.parse(props.initialAns) || {}
+            answers: {}
         }
-    }
-    parseCookies = (ctx) => {
-        return cookie.parse(ctx ? ctx.headers.cookie || {} : document.cookie);
     }
     answers = (ans) => {
         this.state.answers[ans.part] = ans.answer
-        // dispatch(saveInput(this.state.answers));
-        
-        // document.cookie  JSON.stringify(this.state.answer)
-        // // var ab = JSON.parse(document.cookie)=
-        // // console.log(ab)
-        // console.log(document.cookie);
+        this.filterAnswer();
     }
-
     filterAnswer = () => {
+        this.state.filteredAns = {...this.state.answers}
         for (var i = 0; i < this.state.prompts.length; i++) {
             this.state.prompts[i].props.p.answers = this.state.answers[this.state.prompts[i].props.p.part];
         }
@@ -73,39 +47,18 @@ export default class QuestionC extends Component {
             }
             return map;
         }
-        function convertMarkingScheme (markingScheme) { 
-            var map = {};
-            for (let item of markingScheme) {
-                if (item.subparts) {
-                    map[item.part] = item.subparts;
-                    console.log(1)
-                } else {
-                    map[item.part] = item.answer;
-                    console.log(2)
-                }
-            }
-            console.log(map)
-            for (let item of map) {
-                if (Object.prototype.toString.call( item ) === '[object Array]' ) {
-                    //console.log(123);
-                }
-            }
-
-        }
-        var keys = Object.keys(this.state.answers)
+        var keys = Object.keys(this.state.filteredAns)
         for (let i = 0; i < keys.length; i++) {
-            this.state.answers[keys[i]] = this.state.answers[keys[i]][keys[i]];
-            if (this.state.answers[keys[i]] instanceof Set) {
-                var list = convertAnswers(this.state.answers[keys[i]])
-                this.state.answers[keys[i]] = list
+            this.state.filteredAns[keys[i]] = this.state.filteredAns[keys[i]][keys[i]];
+            if (this.state.filteredAns[keys[i]] instanceof Set) {
+                var list = convertAnswers(this.state.filteredAns[keys[i]])
+                this.state.filteredAns[keys[i]] = list
             }
-            //console.log(this.state)
         }
         for (var i = 0; i < this.state.prompts.length; i++) {
-            this.state.prompts[i].props.p.answers = this.state.answers[this.state.prompts[i].props.p.part];
+            this.state.prompts[i].props.p.answers = this.state.filteredAns[this.state.prompts[i].props.p.part];
         }
-        document.cookie  = JSON.stringify(this.state.answers)
-        console.log(document.cookie)
+        document.cookie  = JSON.stringify(this.state.filteredAns)
         const attempt = {
             //question: question_id,
             answer: this.state.answers,
@@ -123,21 +76,7 @@ export default class QuestionC extends Component {
         console.log(this.state)
         this.state.marking_scheme = temp;
         console.log(this.state.marking_scheme);
-        // ans = temp
-        // //console.log(1)
-        // keys = Object.keys(ans)
-        // // console.log(keys)
-        // for (i = 0; i < keys.length; i++) {
-            //console.log(keys[i])
-            //console.log(ans[keys[i]])
-            //console.log(temp)
-            // if (this.marking_scheme[keys[i]])
-            //this.marking_scheme[keys[i]] = convertMarkingScheme(this.marking_scheme[keys[i]]);
-            //console.log(2);
-        //}
     }
-    // funtion modal = new Modal();
-
     render() {
         this.state.marking_scheme = this.props.q.marking_scheme;
         this.state.prompts = this.props.q.content.map(prompt => <Prompt parentCallback = {this.answers} p = {prompt}/>);
@@ -149,7 +88,7 @@ export default class QuestionC extends Component {
                     {(this.props.q.text) ? <TextRenderer text={this.props.q.text}/> : ""}
                     {this.state.prompts}
                 </div>
-                <button onClick= {this.filterAnswer}>Save</button>
+                <button onClick= {console.log(this.state)}>Save</button>
                 <Modal />
             </>
             // <div>{JSON.stringify(this.props.q)}</div>
@@ -157,13 +96,13 @@ export default class QuestionC extends Component {
     }
 }
 
-QuestionC.getInitialProps = async (ctx) => {
-    const res = await fetch(`http://${ctx.req.headers.host}/api/attempts?_id=${ctx.query.question_id}`);
-    const question = (await res.json()).data[0] // Make this an array in the future for better functionality
-    const initAns = cookies(res).answers;
-    console.log(12333)
-    return {
-        question: question,
-        answers: initAns || {}
-    }
-}
+// QuestionC.getInitialProps = async (ctx) => {
+//     const res = await fetch(`http://${ctx.req.headers.host}/api/attempts?_id=${ctx.query.question_id}`);
+//     const question = (await res.json()).data[0] // Make this an array in the future for better functionality
+//     const initAns = cookies(res).answers;
+//     console.log(12333)
+//     return {
+//         question: question,
+//         answers: initAns || {}
+//     }
+// }
