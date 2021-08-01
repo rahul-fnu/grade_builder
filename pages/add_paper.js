33d18833d99f174a6a3a9b137833e01b03b3c668
Alyan <alyan1004@outlook.com>
2021-08-01 21:49:43 +0500

    Fix bugs

--- pages/add_paper.js
+++ pages/add_paper.js
@@ -99,7 +99,7 @@ export default class AddPaperPage extends Component {
                         </select>  
                     </label><br/>
                     <label>Year: </label>
-                        <select className={styles.options} id="year" value = {this.state.year} onChange={(e) => this.handleChang(e)}>
+                        <select className={styles.options} id="year" value = {this.state.year} onChange={(e) => this.handleChange(e)}>
                             <option value="" disabled selected>Select Year</option>
                             <option value="2020">2020</option>
                             <option value="2019">2019</option>
@@ -117,7 +117,7 @@ export default class AddPaperPage extends Component {
                         </select>  
                     <br/>
                     <label>Component Region: </label>
-                        <select className={styles.options} id="component_region" value = {this.state.component_region} onChange={(e) => this.handleChang(e)}>
+                        <select className={styles.options} id="component_region" value = {this.state.component_region} onChange={(e) => this.handleChange(e)}>
                             <option value="" disabled selected>Component Region</option>
                             <option value="11">11</option>
                             <option value="12">12</option>
@@ -140,12 +140,12 @@ export default class AddPaperPage extends Component {
                 <div className = {styles.question_card}>
                     <p>Add Questions</p>
                     {this.state.content}
-                    <button className = {styles.button} disabled = {!this.allValid} onClick={this.addQuestion}>New Question</button>
+                    <button className = {styles.button} disabled = {!this.allValid()} onClick={this.addQuestion}>New Question</button>
                 </div>
                 <div className = {styles.question_card}>
                     <p>Add Marking Scheme</p>
                     {this.state.ms}
-                    <button className = {styles.button} disabled = {!this.allValid} onClick={this.addMS}>New MS Question</button>
+                    <button className = {styles.button} disabled = {!this.allValid()} onClick={this.addMS}>New MS Question</button>
                 </div>
                 <button className = {styles.button} onClick={this.temp}>Submit</button>
             </div>
