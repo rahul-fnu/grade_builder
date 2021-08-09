478b59357c1abf249ed603706f098e1704dea7d5
esim6 <eleanorsimjoann@gmail.com>
2021-08-08 23:32:19 -0700

    Add AWS Cognito

--- pages/login.js
+++ pages/login.js
@@ -3,13 +3,31 @@ import GoogleLogin from 'react-google-login';
 import {Component} from 'react';
 import Route from 'react-router';
 import axios from 'axios';
+import {
+  useAuth,
+  useAuthFunctions,
+  getServerSideAuth,
+} from "../auth";
+
+function withAuth(Component) {
+  return function WrappedComponent(props) {
+    const auth = useAuth(props.initialAuth);
+    const {login, logout} = useAuthFunctions();
+    return <Component {...props} auth={auth} login = {login} logout={logout}/>;
+  }
+}
+
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state =  {
           credentials:{}
         }
-    }  
+        this.auth = props.auth,
+        this.login = props.login,
+        this.logout = props.logout
+    }
+
     responseGoogle = (response) => {
       //console.log(response)
       this.setState({credentials: response});
@@ -60,9 +78,28 @@ class Login extends Component {
           />
           {/* <Route path = "/dashboard/:credentials"
               render = {() => <HomePage credentials = {this.state.credentials.googleID} />} /> */}
+              <React.Fragment>
+                {this.auth ? (
+                <button type="button" onClick={() => this.logout()}>
+                  sign out
+                  </button>) : (
+                  <React.Fragment>
+                    <button type="button" onClick={() => this.login()}>
+                      sign in
+                      </button>
+                      </React.Fragment>
+                      )}
+                      </React.Fragment>
         </div>
       );
     }
 }
 
-export default Login;
+const loginWithAuth = withAuth(Login)
+
+export const getServerSideProps = async (context) => {
+  const initialAuth = getServerSideAuth(context.req);
+  return { props: { initialAuth } };
+};
+
+export default loginWithAuth;
