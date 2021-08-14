84704b98fda8bf77e7d2e7d14a51f82a2ca67793
k19025136 <k19025136@kcl.ac.uk>
2021-08-14 23:45:04 +0200

    Add columns to subject page

--- pages/[board_level]/[subject]/question_card.js
+++ pages/[board_level]/[subject]/question_card.js
@@ -5,8 +5,8 @@ export default class QuestionCard extends Component {
     render() {
         return (
             <div className={styles.card}>
-                <strong>{`${this.props.q.exam_period}, Question ${this.props.q.question_number}`}</strong>
-                <p>{`Topics: ${this.props.q.topics.join(', ')}`}</p>
+                <strong style={{margin: 0}}>{`${this.props.q.exam_period}, Question ${this.props.q.question_number}`}</strong>
+                <p style={{margin: 0}}>{`Topics: ${this.props.q.topics.join(', ')}`}</p>
                 {/* <p>{`Marks: ${this.props.q.marks}`}</p> */}
             </div>
         )
