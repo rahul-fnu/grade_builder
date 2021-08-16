c6af69c754c996cddbda1b0ee2dd1709f8e957f2
esim6 <eleanorsimjoann@gmail.com>
2021-08-16 00:55:25 -0700

    Changed sign out redirect url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         USER_POOL_ID: "us-east-2_98yQmo7sG",
         USER_POOL_CLIENT_ID: "62er3jbeahqrq49l9a0a7iknud",
         REDIRECT_SIGN_IN: "http://localhost:3000/token",
-        REDIRECT_SIGN_OUT: "http://localhost:3000/login",
+        REDIRECT_SIGN_OUT: "http://localhost:3000",
         AUTH_COOKIE_DOMAIN: "localhost"
     }
 }
