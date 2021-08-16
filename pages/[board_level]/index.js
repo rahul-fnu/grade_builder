1838999bbf8df48f3e5faea5752a80a3d5904757
rahul-fnu <rahul-fnu@users.noreply.github.com>
2021-08-15 22:49:55 -0400

    files updated

--- pages/[board_level]/index.js
+++ pages/[board_level]/index.js
@@ -3,3 +3,11 @@ www.gradebuilder.com/board_level/subject/exam_period/component_region/
 www.gradebuilder.com/board_level/subject/topic/question_id/
 
 */
+import { Component } from 'react';
+export default class BoardLevel extends Component {
+    render() {
+        return (
+            <p>This is a dummy page</p>
+        )
+    }
+}
