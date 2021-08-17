302fb88f64e78ec73153d1ab75beb5a3a6571134
rahul-fnu <51520949+rahul-fnu@users.noreply.github.com>
2021-08-17 00:02:39 -0400

    Delete utils directory

--- utils/google_api_keys.js
+++
@@ -1,6 +0,0 @@
-const api_keys = {}
-api_keys.CLIENT_ID = '186327952112-p69c4tfjvpcdoakfbg5frhvs249p2v4d.apps.googleusercontent.com'
-api_keys.CLIENT_SECRET = 'nBsbtgcUX2f0pG4l_Xcp4M5X'
-api_keys.REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-api_keys.REFRESH_TOKEN = '1//04SPoYVVSpBicCgYIARAAGAQSNwF-L9IrWpzAfpb14vJfOnfDd0uIN7KNsKsDt4eZ_i6sDVkTEKavtv1MGp9xYSLNaNcEXwInqSg'
-export default api_keys;
